be551a6f0da94af65e37726e30c07ba39523effc
Mub <84807110+Mub1532@users.noreply.github.com>
2021-11-04 20:33:29 +0000

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,3 @@
-#Windows 95 script, by @UniqueOstrich18 on Replit
-#https://replit.com/@UniqueOstrich18/Win95
-
 if ! command -v qemu-system-x86_64 &> /dev/null
 then
   echo "QEMU not installed, installing now..."
@@ -13,5 +10,5 @@ LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/libraries
 cd resources
 echo "Starting system..."
 
-qemu-system-x86_64 -hda img.qcow -boot d -m 10G -boot a -cdrom arch.iso
-#chromium-browser --no-sandbox
+qemu-system-x86_64 -hda img.qcow -boot d -m 10G -boot a -cdrom arch.iso 
+#change to ur iso

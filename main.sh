d32c369b662939757e76f4bd973931bdd7d9ec42
Mub <84807110+Mub1532@users.noreply.github.com>
2021-11-06 17:03:35 +0000

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 if ! command -v qemu-system-x86_64 &> /dev/null
 then
-  echo "QEMU not installed, installing now..."
+  echo "Installing Qemu because it isnt installed yet"
   sleep 2
   install-pkg qemu
 fi
@@ -8,8 +8,9 @@ fi
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/libraries
 
 cd resources
-echo "Starting system..."
+echo "System is staring..."
 
 qemu-img create -f qcow2 img.qcow 5G
+
 qemu-system-x86_64 -hda img.qcow -boot d -m 4G -boot a -cdrom arch.iso 
-#change to ur iso
+#change arch.iso to ur iso

9d62c68d21dded19f5c8219ee8594b16dea01b7e
Mub <84807110+Mub1532@users.noreply.github.com>
2021-11-06 16:49:09 +0000

    Update main.sh

--- main.sh
+++ main.sh
@@ -10,6 +10,6 @@ LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/libraries
 cd resources
 echo "Starting system..."
 
-qemu-img create img.qcow 5G
+qemu-img create -f qcow2 img.qcow 5G
 qemu-system-x86_64 -hda img.qcow -boot d -m 10G -boot a -cdrom arch.iso 
 #change to ur iso
